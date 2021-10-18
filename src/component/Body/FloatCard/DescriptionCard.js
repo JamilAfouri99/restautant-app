2ce2f357ca615315ca8bde414ac917bf5a4841be
Jamil Afouri <jafouri@nixpend.com>
2021-10-19 00:04:51 +0300

    adding p

--- src/component/Body/FloatCard/DescriptionCard.js
+++ src/component/Body/FloatCard/DescriptionCard.js
@@ -6,7 +6,7 @@ const DescriptionCard = () => {
             <div className="card container" style={{width: '100%',background:'#303030'}}>
                 <div className ="card-body">
                     <h5 className ="card-title">Card title</h5>
-                    {/* <p className ="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem odio id sequi. Nisi ullam commodi, vitae beatae, sed sit amet officia aliquid ab impedit quod iste. Recusandae molestias sed quam et animi, sit nesciunt aspernatur odit facilis repudiandae itaque, voluptas voluptates ad laudantium. Atque magnam commodi blanditiis, debitis laudantium quaerat!</p> */}
+                    <p className ="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem odio id sequi. Nisi ullam commodi, vitae beatae, sed sit amet officia aliquid ab impedit quod iste. Recusandae molestias sed quam et animi, sit nesciunt aspernatur odit facilis repudiandae itaque, voluptas voluptates ad laudantium. Atque magnam commodi blanditiis, debitis laudantium quaerat!</p>
                 </div>
             </div>
         </div>
